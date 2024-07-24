4e24db79ac8d9824e8d4bdcfa975f832303f726b
gmoreira2128 <gabrielmoreira2128@gmail.com>
2024-07-24 16:23:31 -0300

    Alteração do arquivo

--- lista.js
+++ lista.js
@@ -4,3 +4,14 @@ alert("Bem Vindo");
 document.getElementById('btn-msg').addEventListener('click', function(){
     alert("Você clicou no botão");
 })
+
+function VerificaIdade(){
+    var idade = document.getElementById('idade-1').value;
+    var maiorIdade = 18;
+
+    if(idade > maiorIdade){
+        alert("Você é maior de idade")
+    } else{
+        alert("Você não é maior de idade");
+    }
+}
