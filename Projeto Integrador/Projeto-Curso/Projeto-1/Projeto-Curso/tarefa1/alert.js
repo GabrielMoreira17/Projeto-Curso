fdac9ea812bce32c2152f6d41bc0349c082d02a5
Gabriel Moreira <133922923+GabrielMoreira17@users.noreply.github.com>
2025-02-24 13:53:44 -0300

    Delete Projeto Integrador directory

--- Projeto Integrador/Projeto-Curso/Projeto-1/Projeto-Curso/tarefa1/alert.js
+++
@@ -1,5 +0,0 @@
-alert('Bem-Vindo');
-
-document.getElementById('btn-msg').addEventListener('click', function () {
-    alert("Você clicou no botão");
-});
--- Projeto Integrador/pag-inicial/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('btnLogin').addEventListener('click', function(){
-    window.location.href = '../Login/Login.html';
-});
-
-document.getElementById('btnCadastro').addEventListener('click', function(){
-    window.location.href = '../cadastro/cadastro.html';
-});
